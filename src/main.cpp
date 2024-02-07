3a8863ef5197cf4ab5426d379a51151f04166676
Jamason Davis <jpdavis2471@gmail.com>
2024-02-07 10:09:07 -0600

    Updated gitignore, Working primary class and help function. Really need to make a test program for this library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <opencxx-cli/colors.h>
+#include <opencxx-cli/cli.h>
 
 int main() {
+    opencxx_cli::colors cliColor;
     opencxx_cli::CLI cli;
-    cli.printLnColor("Hello World. This is LIGHT", cli.fgBlue);
+    cliColor.printLnColor("Hello World. This is LIGHT", cliColor.fgBlue);
+    struct opencxx_cli::CLI::handler handler;
+    handler.name = "Hello World";
+    cli.help(handler);
     return 0;
 }
